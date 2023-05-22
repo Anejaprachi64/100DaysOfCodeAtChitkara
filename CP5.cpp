a7f0c4f3ea3630c489fddd1296bcfdf6df5ed0d1
Anejaprachi64 <122356899+Anejaprachi64@users.noreply.github.com>
2023-05-22 11:12:23 +0530

    Create CP5.cpp

---
+++ CP5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    for(int i=0;i<T;i++){
+    int X,F;
+    cin>>X;
+    if(X==98)
+    cout<<"NO"<<endl;
+    else if(X>98)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+    }
+    
+	// your code goes here
+	return 0;
+}

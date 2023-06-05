d3a66415c882e6a995c32b330e27962b4beba9f1
Anejaprachi64 <122356899+Anejaprachi64@users.noreply.github.com>
2023-06-05 12:24:47 +0530

    Create CP19.cpp

---
+++ CP19.cpp
@@ -0,0 +1,32 @@
+include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,c=0;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    map<int,int> m;
+	    for(int i=0;i<n;i++){
+	        m[a[i]]++;
+	    }
+	    for(auto &i:m){
+	        if(i.second%2!=0){
+	            c++;
+	            break;
+	    }
+	    
+	    }
+	    if(c>0)cout<<"NO"<<endl;
+	    else cout<<"YES"<<endl;
+	}
+	return 0;
+
+}

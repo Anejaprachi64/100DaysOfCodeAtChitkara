1ea8530117651cd1e387dbe01987a372c2f95f0a
Anejaprachi64 <122356899+Anejaprachi64@users.noreply.github.com>
2023-06-20 20:53:38 +0530

    Create CP34.cpp

---
+++ CP34.cpp
@@ -0,0 +1,18 @@
+#define ll long long
+#define f(end) for(int i=0;i<end;i++)
+#define e '\n'
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+	 cin>>n;
+	 cout<<0-(n+1)<<e;
+	}
+	return 0;
+}

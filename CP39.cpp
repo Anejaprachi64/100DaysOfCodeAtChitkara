c99be6c123beab5ce94ab575342e7e3f2449ccdd
Anejaprachi64 <122356899+Anejaprachi64@users.noreply.github.com>
2023-06-25 20:13:26 +0530

    Create CP39.cpp

---
+++ CP39.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string.h>
+// #include <stdlib.h>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    for (int testcase_variable = 0; testcase_variable < testcase; testcase_variable++)
+    {
+        int a,b,ans=0;
+        cin>>a>>b;
+        if (a%3==0 || b%3==0)
+        {
+            cout<<0<<endl;
+        }
+        else if (a%3==b%3)
+        {
+          
+            cout<<1<<endl;
+        }
+        else
+        {
+            cout<<2<<endl;
+        }
+        
+        
+        
+    }
+}
+
